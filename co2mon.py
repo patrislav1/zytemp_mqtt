2660c5ad906a6984aded122913ab47fad6a5c8ea
Patrick Huesmann <info@patrick-huesmann.de>
2022-09-25 17:42:01 +0200

    Create sensor class

--- co2mon.py
+++ co2mon.py
@@ -1,66 +1,111 @@
 # https://hackaday.io/project/5301-reverse-engineering-a-low-cost-usb-co-monitor
 
 import hid
-from hid import device
+import sys
+import time
 
 CO2_USB_MFG = 'Holtek'
 CO2_USB_PRD = 'USB-zyTemp'
 
-hid_sensors = [
-    e for e in hid.enumerate()
-    if e['manufacturer_string'] == CO2_USB_MFG
-    and e['product_string'] == CO2_USB_PRD
-]
+# Ignore first 5 measurements during self-calibration after power-up
+IGNORE_N_MEASUREMENTS = 5
 
-for s in hid_sensors:
-    intf, path, vid, pid = (s[k] for k in (
-        'interface_number', 'path', 'vendor_id', 'product_id'))
-    path_str = path.decode('utf-8')
-    print(
-        f'Found CO2 sensor at intf. {intf}, {path_str}, VID={vid:04x}, PID={pid:04x}')
 
-h = device()
-h.open_path(path)
+class ZyTemp():
+    MEASUREMENTS = {
+        0x42: {
+            'name': 'Temperature',
+            'unit': '°C',
+            'conversion': lambda x: x / 16 - 273.15,
+        },
+        0x50: {
+            'name': 'CO2',
+            'unit': 'ppm',
+            'conversion': lambda x: x,
+        },
+    }
 
-h.send_feature_report(bytearray(
-    [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]))
+    def __init__(self, hiddev):
+        self.h = hiddev
+        self.h.send_feature_report(b'\xc4\xc6\xc0\x92\x40\x23\xdc\x96')
+        self.measurements_to_ignore = IGNORE_N_MEASUREMENTS
 
-MEASUREMENTS = {
-    0x42: {
-        'name': 'Temperature',
-        'unit': '°C',
-        'conversion': lambda x: x / 16 - 273.15,
-    },
-    0x50: {
-        'name': 'CO2',
-        'unit': 'ppm',
-        'conversion': lambda x: x,
-    },
-}
+    def run(self):
+        while True:
+            try:
+                r = self.h.read(8)
+            except OSError as err:
+                print(f'OS error: {err}')
+                return
 
-while True:
-    r = h.read(8)
-    if not r:
-        print("Read error")
-        continue
+            if not r:
+                print(f'Read error')
+                self.h.close()
+                return
 
-    if r[4] != 0x0d:
-        print("Unexpected data from device")
-        continue
+            if r[4] != 0x0d:
+                print(f'Unexpected data from device')
+                continue
 
-    if r[3] != sum(r[0:3]) & 0xff:
-        print("Checksum error")
-        continue
+            if r[3] != sum(r[0:3]) & 0xff:
+                print(f'Checksum error')
+                continue
 
-    m_type = r[0]
-    m_val = r[1] << 8 | r[2]
+            m_type = r[0]
+            m_val = r[1] << 8 | r[2]
 
-    try:
-        m = MEASUREMENTS[m_type]
-    except KeyError:
-        #        print(f'Unknown key {m_type:02x}')
-        continue
+            try:
+                m = ZyTemp.MEASUREMENTS[m_type]
+            except KeyError:
+                #        print(f'Unknown key {m_type:02x}')
+                continue
 
-    m_name, m_unit, m_reading = m['name'], m['unit'], m['conversion'](m_val)
+            m_name, m_unit, m_reading = m['name'], m['unit'], m['conversion'](
+                m_val)
 
-    print(f'{m_name}: {m_reading:g} {m_unit}')
+            ignore = self.measurements_to_ignore > 0
+
+            print(f'{m_name}: {m_reading:g} {m_unit}' +
+                  (' (ignored)' if ignore else ''))
+
+            if m_name == 'CO2' and self.measurements_to_ignore:
+                self.measurements_to_ignore -= 1
+
+
+def get_dev():
+    hid_sensors = [
+        e for e in hid.enumerate()
+        if e['manufacturer_string'] == CO2_USB_MFG
+        and e['product_string'] == CO2_USB_PRD
+    ]
+
+    p = []
+    for s in hid_sensors:
+        intf, path, vid, pid = (
+            s[k] for k in
+            ('interface_number', 'path', 'vendor_id', 'product_id')
+        )
+        path_str = path.decode('utf-8')
+        print(
+            f'Found CO2 sensor at intf. {intf}, {path_str}, VID={vid:04x}, PID={pid:04x}')
+        p.append(path)
+
+    if not p:
+        print('No device found', file=sys.stderr)
+        return None
+
+    print(f'Using device at {p[0].decode("utf-8")}')
+    h = hid.device()
+    h.open_path(path)
+
+    return ZyTemp(h)
+
+
+if __name__ == '__main__':
+    while True:
+        zt = get_dev()
+        if not zt:
+            time.sleep(5)
+            continue
+        zt.run()
+        time.sleep(5)
